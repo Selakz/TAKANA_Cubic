b4473bf06bc7d1eafde3819d9b5e13e922481f9d
Selakz <787276422@qq.com>
2025-10-03 21:19:22 +0800

    fix: add back cover loading

---
+++ Assets/Scripts/MusicGame/Gameplay/Level/UI/CoverLoader.cs
@@ -0,0 +1,31 @@
+#nullable enable
+
+using T3Framework.Runtime.Event;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MusicGame.Gameplay.Level.UI
+{
+	public class CoverLoader : MonoBehaviour
+	{
+		// Serializable and Public
+		[SerializeField] private RawImage coverImage = default!;
+
+		// Event Handlers
+		private void LevelOnLoad(LevelInfo levelInfo)
+		{
+			coverImage.texture = levelInfo.Cover;
+		}
+
+		// System Functions
+		void OnEnable()
+		{
+			EventManager.Instance.AddListener<LevelInfo>("Level_OnLoad", LevelOnLoad);
+		}
+
+		void OnDisable()
+		{
+			EventManager.Instance.RemoveListener<LevelInfo>("Level_OnLoad", LevelOnLoad);
+		}
+	}
+}
