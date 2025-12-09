4a5bd96369274016904c7fb5872c0f7dae24509d
Selakz <787276422@qq.com>
2025-12-09 14:26:35 +0800

    refactor: remove EventTags

--- Assets/Scripts/T3Framework/Runtime/T3MonoBehaviour.cs
+++ Assets/Scripts/T3Framework/Runtime/T3MonoBehaviour.cs
@@ -1,7 +1,6 @@
 #nullable enable
 
 using System;
-using System.Collections.Generic;
 using T3Framework.Runtime.Event;
 using UnityEngine;
 
@@ -11,48 +10,17 @@ namespace T3Framework.Runtime
 	public abstract class T3MonoBehaviour : MonoBehaviour
 	{
 		// Serializable and Public
-		[SerializeField] private string[] inspectorTags = { };
-
-		/// <summary> Will only be called once when <see cref="T3MonoBehaviour.Tags"/> is constructed </summary>
-		protected virtual string[] PresetTags => new string[] { };
-
-		/// <summary> A single tag of GameObject should never be enough! </summary>
-		public EventTags Tags
-		{
-			get
-			{
-				if (tags is null)
-				{
-					var initialTags = new HashSet<string>();
-					initialTags.UnionWith(inspectorTags);
-					initialTags.UnionWith(PresetTags);
-					tags = new(gameObject, initialTags);
-				}
-
-				return tags;
-			}
-		}
-
 		protected virtual IEventRegistrar[] AwakeRegistrars { get; } = Array.Empty<IEventRegistrar>();
 
 		protected virtual IEventRegistrar[] EnableRegistrars { get; } = Array.Empty<IEventRegistrar>();
 
 		// Private
-		private EventTags? tags;
 		private IEventRegistrar[]? awakeRegistrars;
 		private IEventRegistrar[]? enableRegistrars;
 
 		// System Functions
 		protected virtual void Awake()
 		{
-			if (tags is null)
-			{
-				var initialTags = new HashSet<string>();
-				initialTags.UnionWith(inspectorTags);
-				initialTags.UnionWith(PresetTags);
-				tags = new(gameObject, initialTags);
-			}
-
 			awakeRegistrars ??= AwakeRegistrars;
 			foreach (var registrar in awakeRegistrars) registrar.Register();
 		}
@@ -73,8 +41,6 @@ namespace T3Framework.Runtime
 		{
 			awakeRegistrars ??= AwakeRegistrars;
 			foreach (var registrar in awakeRegistrars) registrar.Unregister();
-
-			tags?.Dispose();
 		}
 	}
 }
