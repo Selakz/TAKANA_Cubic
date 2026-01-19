e64f55b8bce16ef4dddc8f8eec7cf6f8b67e5085
Selakz <787276422@qq.com>
2026-01-19 11:55:49 +0800

    fix: disappeared notes can still be selected

--- Assets/Scripts/MusicGame/ChartEditor/Select/SelectColliderPluginSystem.cs
+++ Assets/Scripts/MusicGame/ChartEditor/Select/SelectColliderPluginSystem.cs
@@ -1,18 +1,21 @@
 #nullable enable
 
 using System.Collections.Generic;
+using MusicGame.Gameplay.Audio;
 using MusicGame.Gameplay.Chart;
 using MusicGame.Models;
 using T3Framework.Runtime.ECS;
 using T3Framework.Runtime.Event;
 using VContainer;
+using VContainer.Unity;
 
 namespace MusicGame.ChartEditor.Select
 {
-	public class SelectColliderPluginSystem : T3System
+	public class SelectColliderPluginSystem : T3System, ITickable
 	{
 		// Private
 		private readonly IClassifier<T3Flag> classifier;
+		private readonly IGameAudioPlayer music;
 		private readonly IViewPool<ChartComponent> viewPool;
 		private readonly IViewPool<ChartComponent> pluginPool;
 		private readonly Dictionary<T3Flag, TextureAlignInfo> textureAlignInfos;
@@ -31,6 +34,8 @@ namespace MusicGame.ChartEditor.Select
 							var plugin = handler!.Script<SelectColliderPlugin>();
 							plugin.gameObject.layer = pair.Value.colliderLayer;
 							plugin.StartAligning(pair.Value);
+							plugin.ColliderModifier.EnabledModifier.Register(
+								_ => data.Model.TimeMax > music.ChartTime, 1);
 						}
 					}
 				},
@@ -39,14 +44,28 @@ namespace MusicGame.ChartEditor.Select
 
 		// Defined Functions
 		public SelectColliderPluginSystem(
+			IGameAudioPlayer music,
 			[Key("stage")] IViewPool<ChartComponent> viewPool,
 			[Key("select-collider")] IViewPool<ChartComponent> pluginPool,
 			[Key("select-collider")] Dictionary<T3Flag, TextureAlignInfo> textureAlignInfos) : base(true)
 		{
 			classifier = new T3ChartClassifier();
+			this.music = music;
 			this.viewPool = viewPool;
 			this.pluginPool = pluginPool;
 			this.textureAlignInfos = textureAlignInfos;
+		}
+
+		// System Functions
+		public void Tick()
+		{
+			foreach (var component in pluginPool)
+			{
+				if (pluginPool[component]?.Script<SelectColliderPlugin>() is { } plugin)
+				{
+					plugin.ColliderModifier.EnabledModifier.Update();
+				}
+			}
 		}
 	}
 }
