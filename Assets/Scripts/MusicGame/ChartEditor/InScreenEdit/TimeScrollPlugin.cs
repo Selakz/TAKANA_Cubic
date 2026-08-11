f51f8333293f4686fcaa60fd7cde962460ecb4fb
Selakz <787276422@qq.com>
2026-08-11 22:07:22 +0800

    fix: time scrolling is now always on even when grid is not on.

--- Assets/Scripts/MusicGame/ChartEditor/InScreenEdit/TimeScrollPlugin.cs
+++ Assets/Scripts/MusicGame/ChartEditor/InScreenEdit/TimeScrollPlugin.cs
@@ -1,6 +1,5 @@
 #nullable enable
 
-using System;
 using MusicGame.ChartEditor.InScreenEdit.Grid;
 using MusicGame.ChartEditor.Level;
 using MusicGame.Gameplay.Audio;
@@ -9,36 +8,22 @@ using T3Framework.Runtime.Input;
 using T3Framework.Runtime.Setting;
 using T3Framework.Runtime.VContainer;
 using T3Framework.Static;
-using T3Framework.Static.Event;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using VContainer;
-using VContainer.Unity;
 
 namespace MusicGame.ChartEditor.InScreenEdit
 {
-	public class TimeScrollPlugin : MonoBehaviour, ISelfInstaller
+	public class TimeScrollPlugin : HierarchySystem<TimeScrollPlugin>
 	{
+		// Serializable and Public
+		[SerializeField] private float scrollTweenDuration = 0.1f;
+
 		// Private
-		private Camera levelCamera = default!;
-		private IGameAudioPlayer music = default!;
-		private NotifiableProperty<ITimeRetriever> timeRetriever = default!;
-
-		// Defined Functions
-		[Inject]
-		private void Construct(
-			[Key("stage")] Camera levelCamera,
-			IGameAudioPlayer music,
-			NotifiableProperty<ITimeRetriever> timeRetriever)
-		{
-			this.levelCamera = levelCamera;
-			this.music = music;
-			this.timeRetriever = timeRetriever;
-		}
-
-		public void SelfInstall(IContainerBuilder builder)
-			=> builder.RegisterComponent(this).Keyed(Guid.NewGuid().ToString());
-
+		[Inject, Key("stage")] private Camera levelCamera = default!;
+		[Inject] private IGameAudioPlayer music = default!;
+		[Inject] private GridTimeRetriever gridTimeRetriever = default!;
+		
 		// System Functions
 		void Update()
 		{
@@ -46,41 +31,38 @@ namespace MusicGame.ChartEditor.InScreenEdit
 			if (y == 0) return;
 
 			// Manually judge
-			if (!ISingleton<InputManager>.Instance.GlobalInputEnabled || music.Clip == null) return;
-
-			if (levelCamera.ContainsScreenPoint(Mouse.current.position.ReadValue()) &&
-			    timeRetriever.Value is GridTimeRetriever gridTimeRetriever)
+			if (!ISingleton<InputManager>.Instance.GlobalInputEnabled ||
+			    music.Clip == null ||
+			    !levelCamera.ContainsScreenPoint(Mouse.current.position.ReadValue())) return;
+			
+			var current = music.ChartTime;
+			var scrollSensitivity = ISingletonSetting<EditorSetting>.Instance.ScrollSensitivity.Value;
+			bool forward = y * scrollSensitivity > 0;
+			// Forced to have a liminality to prevent endless loop...
+			for (int i = 0, liminal = 0; i < scrollSensitivity && liminal < 100; i++)
 			{
-				var current = music.ChartTime;
-				var scrollSensitivity = ISingletonSetting<EditorSetting>.Instance.ScrollSensitivity.Value;
-				bool forward = y * scrollSensitivity > 0;
-				scrollSensitivity = Mathf.Abs(scrollSensitivity);
-				// Forced to have a liminality to prevent endless loop...
-				for (int i = 0, liminal = 0; i < scrollSensitivity && liminal < 100; i++)
+				var next = forward
+					? gridTimeRetriever.GetCeilTime(current)
+					: gridTimeRetriever.GetFloorTime(current);
+				if (next <= 0 || next > music.AudioLength)
 				{
-					var next = forward
-						? gridTimeRetriever.GetCeilTime(current)
-						: gridTimeRetriever.GetFloorTime(current);
-					if (next <= 0 || next > music.AudioLength)
-					{
-						current = next;
-						break;
-					}
-
-					if (Mathf.Abs(next - current) <= 1)
-					{
-						i--;
-						liminal++;
-						current = next + (forward ? liminal : -liminal);
-					}
-					else
-					{
-						current = next;
-					}
+					current = next;
+					break;
 				}
 
-				music.ChartTime = current;
+				if (Mathf.Abs(next - current) <= 1)
+				{
+					i--;
+					liminal++;
+					current = next + (forward ? liminal : -liminal);
+				}
+				else
+				{
+					current = next;
+				}
 			}
+
+			music.ChartTime = current;
 		}
 	}
 }
